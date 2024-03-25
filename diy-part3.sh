1f38d57e2294d8c7f90ccacb8aa3768fe1849260
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-25 18:57:43 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 
 #删除默认登陆密码
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
