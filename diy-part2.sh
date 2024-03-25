a0c3b01f22bc21f3669a17eaaffb5769b7e2cd0c
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-25 19:29:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,8 +21,8 @@ sed -i 's/ImmortalWrt/RAX3000M/g' package/base-files/files/bin/config_generate
 #sed -i 's/ImmortalWrt-5G/RAX3000M_5G/g' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 #修改闪存为256M版本(这是针对原厂128闪存来的，但又要编译256M固件来的）
-sed -i 's/<0x580000 0x7200000>/<0x580000 0xee00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
-sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
+#sed -i 's/<0x580000 0x7200000>/<0x580000 0xee00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7981-cmcc-rax3000m.dts
+#sed -i 's/116736k/240128k/g' target/linux/mediatek/image/mt7981.mk
 
 #删除冲突的软件包
 #rm -rf ./package/istore
